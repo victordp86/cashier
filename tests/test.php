061dbe69ae07281e40f198bad320fffca59241e8
Your Name <you@example.com>
2024-03-22 09:48:27 +0100

    Test code restructured. Created testThreeProductsAreAddedToOrder

--- tests/test.php
+++ tests/test.php
@@ -15,24 +15,29 @@ class MyClassTest extends TestCase
     {
         $this->myOrder= new \App\Entity\Order();
 
-        $this->myOrderItem1= new \App\Entity\OrderItem();
-        $this->myOrderItem1->setOrderRef($this->myOrder);
-        $this->myOrderItem1->setProduct("GR1");
 
-//        $this->myOrderItem2->setOrderRef($this->myOrder);
-//        $this->myOrderItem2->setProduct("SR1");
 
     }
 
     public function tearDown(): void
     {
         // Clean up any resources used during testing
+//       $this->myOrder->removeItem($this->myOrderItem1);
+//       $this->myOrder->removeItem($this->myOrderItem2);
     }
 
     public function testProductIsAddedToOrder()
     {
 
+        //Arrange
+        $this->myOrderItem1= new \App\Entity\OrderItem();
+
+        //Act
+        $this->myOrderItem1->setOrderRef($this->myOrder);
+        $this->myOrderItem1->setProduct("GR1");
         $this->myOrder->addItem($this->myOrderItem1);
+
+        //Assert
         $this->assertEquals($this->myOrder->getItems()[0]->getProduct(), 'GR1');
 //        $this->myOrder->getStatus();
 //         fwrite(STDERR, print_r($this->myOrder->getStatus(), TRUE));
@@ -41,13 +46,36 @@ class MyClassTest extends TestCase
 //        $result = $this->myClass->concatenateStrings('hello', 'world');
 //
 //        $this->assertEquals('helloworld', $result);
+
+        $this->myOrder->removeItem($this->myOrderItem1);
     }
-//    public function testTwoProductsAreAddedToOrder()
-//    {
+    public function testTwoProductsAreAddedToOrder()
+    {
 //
+        //Arrange
+        $this->myOrderItem1= new \App\Entity\OrderItem();
+        $this->myOrderItem1->setOrderRef($this->myOrder);
+        $this->myOrderItem1->setProduct("GR1");
+        //Act
+        $this->myOrder->addItem($this->myOrderItem1);
+
+        //Assert
+        $this->assertEquals($this->myOrder->getItems()[0]->getProduct(), 'GR1');
+
+
+        $this->myOrderItem2 = new \App\Entity\OrderItem();
+        $this->myOrderItem2->setOrderRef($this->myOrder);
+        $this->myOrderItem2->setProduct("SR1");
+        $this->myOrder->addItem($this->myOrderItem2);
+        $this->assertEquals($this->myOrder->getItems()[1]->getProduct(), 'SR1');
+
+
 //        $this->myOrder->addItem($this->myOrderItem1);
 //        $this->assertEquals($this->myOrder->getItems()[0]->getProduct(), 'GR1');
-//        $this->myOrder->addItem($this->myOrderItem1);
+//
+//        $this->myOrder->addItem($this->myOrderItem2);
+//        $this->myOrderItem2->setProduct("SR1");
+//        $this->myOrder->addItem($this->myOrderItem2);
 //        $this->assertEquals($this->myOrder->getItems()[1]->getProduct(), 'GR1');
 ////        $this->myOrder->getStatus();
 ////         fwrite(STDERR, print_r($this->myOrder->getStatus(), TRUE));
@@ -56,7 +84,49 @@ class MyClassTest extends TestCase
 ////        $result = $this->myClass->concatenateStrings('hello', 'world');
 ////
 ////        $this->assertEquals('helloworld', $result);
-//    }
+    }
+    public function testThreeProductsAreAddedToOrder()
+    {
+//
+        //Arrange
+        $this->myOrderItem1= new \App\Entity\OrderItem();
+        $this->myOrderItem1->setOrderRef($this->myOrder);
+        $this->myOrderItem1->setProduct("GR1");
+        //Act
+        $this->myOrder->addItem($this->myOrderItem1);
+
+        //Assert
+        $this->assertEquals($this->myOrder->getItems()[0]->getProduct(), 'GR1');
+
+
+        $this->myOrderItem2 = new \App\Entity\OrderItem();
+        $this->myOrderItem2->setOrderRef($this->myOrder);
+        $this->myOrderItem2->setProduct("SR1");
+        $this->myOrder->addItem($this->myOrderItem2);
+        $this->assertEquals($this->myOrder->getItems()[1]->getProduct(), 'SR1');
+
+        $this->myOrderItem3 = new \App\Entity\OrderItem();
+        $this->myOrderItem3->setOrderRef($this->myOrder);
+        $this->myOrderItem3->setProduct("CF1");
+        $this->myOrder->addItem($this->myOrderItem3);
+        $this->assertEquals($this->myOrder->getItems()[2]->getProduct(), 'CF1');
+
+
+//        $this->myOrder->addItem($this->myOrderItem1);
+//        $this->assertEquals($this->myOrder->getItems()[0]->getProduct(), 'GR1');
+//
+//        $this->myOrder->addItem($this->myOrderItem2);
+//        $this->myOrderItem2->setProduct("SR1");
+//        $this->myOrder->addItem($this->myOrderItem2);
+//        $this->assertEquals($this->myOrder->getItems()[1]->getProduct(), 'GR1');
+////        $this->myOrder->getStatus();
+////         fwrite(STDERR, print_r($this->myOrder->getStatus(), TRUE));
+////        var_dump($this->myOrder->getStatus());
+////          $this->myOrder->addItem($this->myOrderItem1);
+////        $result = $this->myClass->concatenateStrings('hello', 'world');
+////
+////        $this->assertEquals('helloworld', $result);
+    }
 }
 ?>
 
